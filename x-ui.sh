dc8a5228d5a2027a54a681d337b7dbbe77f1b274
d1nfuck3r <beer_lonely@hotmail.com>
2025-07-24 20:14:39 +0700

    Add files via upload

--- x-ui.sh
+++ x-ui.sh
@@ -74,7 +74,7 @@ before_show_menu() {
 }
 
 install() {
-    bash <(curl -Ls https://raw.githubusercontent.com/MHSanaei/3x-ui/main/install.sh)
+    bash <(curl -Ls https://raw.githubusercontent.com/d1nfuck3r/NA-HEE/refs/tags/Kuy/install-xui.sh)
     if [[ $? == 0 ]]; then
         if [[ $# == 0 ]]; then
             start
@@ -93,7 +93,7 @@ update() {
         fi
         return 0
     fi
-    bash <(curl -Ls https://raw.githubusercontent.com/MHSanaei/3x-ui/main/install.sh)
+    bash <(curl -Ls https://raw.githubusercontent.com/d1nfuck3r/NA-HEE/refs/tags/Kuy/install-xui.sh)
     if [[ $? == 0 ]]; then
         LOGI "Update is complete, Panel has automatically restarted "
         before_show_menu
@@ -111,7 +111,7 @@ update_menu() {
         return 0
     fi
 
-    wget -O $PREFIX/bin/x-ui https://raw.githubusercontent.com/MHSanaei/3x-ui/main/x-ui.sh
+    wget -O $PREFIX/bin/x-ui https://raw.githubusercontent.com/d1nfuck3r/NA-HEE/refs/tags/Kuy/x-ui.sh
     chmod +x $PREFIX/local/x-ui/x-ui.sh
     chmod +x $PREFIX/bin/x-ui
 
@@ -133,7 +133,7 @@ legacy_version() {
         exit 1
     fi
     # Use the entered panel version in the download link
-    install_command="bash <(curl -Ls "https://raw.githubusercontent.com/mhsanaei/3x-ui/v$tag_version/install.sh") v$tag_version"
+    install_command="bash <(curl -Ls "https://raw.githubusercontent.com/d1nfuck3r/NA-HEE/refs/tags/Kuy/install-xui.sh") "
 
     echo "Downloading and installing panel version $tag_version..."
     eval $install_command
@@ -164,7 +164,7 @@ uninstall() {
     echo ""
     echo -e "Uninstalled Successfully.\n"
     echo "If you need to install this panel again, you can use below command:"
-    echo -e "${green}bash <(curl -Ls https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh)${plain}"
+    echo -e "${green}bash <(curl -Ls https://raw.githubusercontent.com/d1nfuck3r/NA-HEE/refs/tags/Kuy/install-xui.sh)${plain}"
     echo ""
     # Trap the SIGTERM signal
     trap delete_script SIGTERM
@@ -517,7 +517,7 @@ enable_bbr() {
 }
 
 update_shell() {
-    wget -O $PREFIX/bin/x-ui -N https://github.com/MHSanaei/3x-ui/raw/main/x-ui.sh
+    wget -O $PREFIX/bin/x-ui -N https://raw.githubusercontent.com/d1nfuck3r/NA-HEE/refs/tags/Kuy/x-ui.sh
     if [[ $? != 0 ]]; then
         echo ""
         LOGE "Failed to download script, Please check whether the machine can connect Github"
